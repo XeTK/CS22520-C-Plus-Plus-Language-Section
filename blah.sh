fad165919fd1d390f0eb93fc4b1bbb3c747ecf55
Tom Rosier <xetk@xetk-iMac.(none)>
2013-03-14 22:42:52 +0000

    fixed netbeans

--- blah.sh
+++
@@ -1,2 +0,0 @@
-rm -v simple
-g++ -o simple main.cpp Gui.cpp Gui.h Node.cpp Node.h Course.cpp Course.h Entrant.cpp Entrant.h Holder.cpp Holder.h FileReader.cpp FileReader.h `pkg-config --libs --cflags gtk+-2.0`
