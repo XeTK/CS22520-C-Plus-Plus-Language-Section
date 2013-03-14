6bf8a1a3e3c3234314bb307ea6dd6ff912002f25
Tom Rosier <xetk@xetk-iMac.(none)>
2013-03-14 21:18:05 +0000

    Fixing

--- FileReader.cpp
+++ FileReader.cpp
@@ -6,15 +6,26 @@
  */
 #include <vector>
 #include <string>
+#include <iostream>
+#include <fstream>
 #include "FileReader.h"
 using namespace std;
 vector<string> FileReader::get_file_contents(string path)
 {
-    
+    vector<string> temp_strings;
+    string temp;
+    ifstream infile(path.c_str());
+    while(!infile.eof())
+    {
+        getline(infile,temp);
+        temp_strings.push_back(temp);
+    }
+    infile.close();
+    return temp_strings;
 }
 vector<Course> FileReader::get_courses(string path)
 {
-    
+    get_file_contents(path);
 }
 vector<Entrant> FileReader::get_entrants(string path)
 {
--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <gtk/gtk.h>
 #include "Gui.h"
+#include "FileReader.h"
 using namespace std;
 int main(int argc, char* argv[])
 {
-        printf("%s",argv[1]);
         gtk_init(&argc, &argv);
 	Gui g;
 	g.Load();
-	return 0;
+    //FileReader fr;
+    //fr.get_courses("Data/courses.txt");
+        return 0;
 } 
 
